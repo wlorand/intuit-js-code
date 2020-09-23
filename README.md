4e035dd7a9967526279dc010aa8f4ac28ed4681f
Will Lorand <bill@milemarker3.org>
2020-09-23 10:58:13 -0700

    refactored to add test runs, default param, package.json and gitignore

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # mydata-js-code
 
+### JS Utility Code Samples from the My Data & Privacy Card SPA at accounts.intuit.com (2019)
+
 1- intuitProductSort.js
--- intuitProductSort.js
+++ intuitProductSort.js
@@ -4,23 +4,41 @@
  *     listing aid entry first if present from state.queryParams
  */
 
-export const rawProductList = [
+const rawProductList = [
   { productName: 'TurboTax', productId: 'tt' },
   { productName: 'Turbo', productId: 'tb' },
-  { productName: 'Mint', productId: 'mt' }
+  { productName: 'Mint', productId: 'mt' },
 ];
 
-export function sortProductList(productList, aid) {
+function sortProductList(productList, aid = 0) {
   let finalProductList = [];
-  const sortedProductList = productList.sort((a, b) => a.productName > b.productName ? 1 : -1);
+  const sortedProductList = productList.sort((a, b) =>
+    a.productName > b.productName ? 1 : -1
+  );
   if (!aid) {
     finalProductList = sortedProductList;
   } else {
     // grab the aid product, remove it from the list and create a new array with the aid product listed first
     // TODO: refactor to only do one iteration thru
-    const aidProduct = sortedProductList.filter(product => product.productId === aid);
-    const productListNoAid = sortedProductList.filter(product => product.productId !== aid);
+    const aidProduct = sortedProductList.filter(
+      (product) => product.productId === aid
+    );
+
+    const productListNoAid = sortedProductList.filter(
+      (product) => product.productId !== aid
+    );
+    // use array spread to get products in final order
     finalProductList = [...aidProduct, ...productListNoAid];
   }
   return finalProductList;
 }
+
+// test run
+console.log(sortProductList(rawProductList));
+
+// test run with aid='tt' (turbotax first)
+console.log(sortProductList(rawProductList, 'tt'));
+
+// using node style exports
+exports.rawProductList = rawProductList;
+exports.sortProductList = sortProductList;
